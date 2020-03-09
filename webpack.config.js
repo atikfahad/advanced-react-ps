68220e7b285f73bcc40fc16b6bc3eba66941be71
Atik Fahad <atikfahad@Atiks-MacBook-Pro.local>
2020-03-10 02:48:53 +0600

    set up react but couldn add babel in sever

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './lib/components/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
+  }
+};
